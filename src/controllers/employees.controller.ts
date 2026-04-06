27edb8e7130551ec5cd641e7cef0d00324277954
PressureDraper <warmarines00@hotmail.com>
2026-04-06 13:28:37 -0600

    chore(files): rename

--- src/controllers/employees.controller.ts
+++ src/controllers/employees.controller.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from 'express';
 import { EmployeeService } from '../services/employees.service.js';
-import client from '../lib/redis.utils.js';
+import client from '../lib/redis.js';
 
 export class EmployeeController {
     private employeeService: EmployeeService;
